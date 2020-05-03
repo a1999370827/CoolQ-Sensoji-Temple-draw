93d25c30d5e819d4e213110ff4263c6872714acb
a1999370827 <64728991+a1999370827@users.noreply.github.com>
2020-05-03 17:56:46 +0800

    Update CQHandler.py

--- xyz.shinnshi.chouqian/CQHandler.py
+++ xyz.shinnshi.chouqian/CQHandler.py
@@ -37,25 +37,15 @@ class CQHandler(object):
     def OnEvent_PrivateMsg(self, subType, sendTime, fromQQ, msg, font):
         global last_time
         logging.info('OnEvent_PrivateMsg: subType={0}, sendTime={1}, fromQQ={2}, msg={3}, font={4}'.format(subType, sendTime, fromQQ, msg, font))
-        if msg == "抽签":
+        if msg == "鎶界":
             try:
                 folder = os.path.exists("./data/app/xyz.shinnshi.chouqian")
                 if not folder:
                     os.makedirs("./data/app/xyz.shinnshi.chouqian")
                 if not (os.path.exists("./data/app/xyz.shinnshi.chouqian/today.txt")):
                     with open("./data/app/xyz.shinnshi.chouqian/today.txt","w") as f:
-                        f.write('当前已抽：\n')
-                now_time = time.strftime('%Y-%m-%d',time.localtime())
-                if last_time != now_time:
-                    with open("./data/app/xyz.shinnshi.chouqian/today.txt","w") as f:
-                        f.write('当前已抽：\n')
-                        last_time = now_time
-                with open("./data/app/xyz.shinnshi.chouqian/today.txt","r") as f:
-                    today=[]
-                    today=f.readlines()
-                    qq = str(fromQQ)+"\n"
-                    if  qq in today:
-                        CQSDK.SendPrivateMsg(fromQQ, "你今天已经抽过了哦，明天再来吧~")
+                        f.write('褰撳墠宸叉娊锛歕n')
+                now_time = time.strftime('%Y-%m-%d',time.localtime())               
                         return
                 id = random.randint(1,100)
                 msg = chouqian(id).decode('UTF-8').encode('GBK')
@@ -67,25 +57,15 @@ class CQHandler(object):
     def OnEvent_GroupMsg(self, subType, sendTime, fromGroup, fromQQ, fromAnonymous, msg, font):
         global last_time
         logging.info('OnEvent_GroupMsg: subType={0}, sendTime={1}, fromGroup={2}, fromQQ={3}, fromAnonymous={4}, msg={5}, font={6}'.format(subType, sendTime, fromGroup, fromQQ, fromAnonymous, msg, font))
-        if msg == "抽签":
+        if msg == "鎶界":
             try:
                 folder = os.path.exists("./data/app/xyz.shinnshi.chouqian")
                 if not folder:
                     os.makedirs("./data/app/xyz.shinnshi.chouqian")
                 if not (os.path.exists("./data/app/xyz.shinnshi.chouqian/today.txt")):
                     with open("./data/app/xyz.shinnshi.chouqian/today.txt","w") as f:
-                        f.write('当前已抽：\n')
-                now_time = time.strftime('%Y-%m-%d',time.localtime())
-                if last_time != now_time:
-                    with open("./data/app/xyz.shinnshi.chouqian/today.txt","w") as f:
-                        f.write('当前已抽：\n')
-                        last_time = now_time
-                with open("./data/app/xyz.shinnshi.chouqian/today.txt","r") as f:
-                    today=[]
-                    today=f.readlines()
-                    qq = str(fromQQ)+"\n"
-                    if  qq in today:
-                        CQSDK.SendGroupMsg(fromGroup, "你今天已经抽过了哦，明天再来吧~")
+                        f.write('褰撳墠宸叉娊锛歕n')
+                now_time = time.strftime('%Y-%m-%d',time.localtime())               
                         return
                 id = random.randint(1,100)
                 msg = chouqian(id).decode('UTF-8').encode('GBK')
